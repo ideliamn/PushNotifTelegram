253ea009eb427361329df23f107a9ab89d73702e
Idelia Muthia <ideliamuthia@gmail.com>
2025-08-07 16:30:50 +0700

    initial commit

---
+++ PushNotifTelegram/Controllers/NotifController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PushNotifTelegram.Services;
+
+namespace PushNotifTelegram.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class NotifController : Controller
+    {
+        private readonly TelegramServices _telegramServices;
+        public NotifController(TelegramServices telegramServices)
+        {
+            _telegramServices = telegramServices;
+        }
+
+        [HttpPost("send")]
+        public async Task<IActionResult> SendNotif([FromBody] string message)
+        {
+            await _telegramServices.SendMessage(message);
+            return Ok("Success send message");
+        }
+    }
+}
---
+++ PushNotifTelegram/Services/TelegramServices.cs
@@ -0,0 +1,28 @@
+﻿namespace PushNotifTelegram.Services
+{
+    public class TelegramServices
+    {
+        public readonly HttpClient _httpClient;
+        public TelegramServices()
+        {
+            _httpClient = new HttpClient();
+        }
+        public async Task SendMessage(string message)
+        {
+            var _botToken = "8026778612:AAFidn2nPL_M1lOITK5wG5UfkMlSbrArAOU";
+            var _chatId = "7248325894";
+
+            var url = $"https://api.telegram.org/bot{_botToken}/sendMessage";
+
+            var payload = new Dictionary<string, string>
+            {
+                { "chat_id", _chatId },
+                { "text", message }
+            };
+
+            var content = new FormUrlEncodedContent(payload);
+            var response = await _httpClient.PostAsync(url, content);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
